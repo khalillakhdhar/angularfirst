996fca4d5fb2a68750ae0d1db76fd8f07f24f222
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2024-04-21 10:37:10 +0100

    application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularComponent } from './angular/angular.component';
+import { HomeComponent } from './home/home.component';
+import { ReactComponent } from './react/react.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'angular',component:AngularComponent},
+  {path:'react',component:ReactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ReactComponent } from './react/react.component';
+import { AngularComponent } from './angular/angular.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ReactComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule,
